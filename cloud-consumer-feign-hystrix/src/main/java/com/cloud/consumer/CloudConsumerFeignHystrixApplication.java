c809d882bee9879e6f9e26a11d72977c0d69d8a4
lee <504861378@qq.com>
2019-04-22 16:41:50 +0800

    add HystrixCommand and TestRule

--- cloud-consumer-feign-hystrix/src/main/java/com/cloud/consumer/CloudConsumerFeignHystrixApplication.java
+++ cloud-consumer-feign-hystrix/src/main/java/com/cloud/consumer/CloudConsumerFeignHystrixApplication.java
@@ -2,17 +2,28 @@ package com.cloud.consumer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @EnableEurekaClient
 @EnableHystrixDashboard
 @EnableFeignClients
 @EnableHystrix
+@RibbonClient(name = "cloud-provider", configuration = com.cloud.config.LoadBalanced.class)
 public class CloudConsumerFeignHystrixApplication {
+
+	@Bean
+	@LoadBalanced // 开启负载均衡
+	public RestTemplate restTemplate() {
+		return new RestTemplate();
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(CloudConsumerFeignHystrixApplication.class, args);
--- cloud-consumer-feign-hystrix/src/main/java/com/cloud/consumer/controller/UserController.java
+++ cloud-consumer-feign-hystrix/src/main/java/com/cloud/consumer/controller/UserController.java
@@ -5,9 +5,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 import com.cloud.consumer.domain.User;
 import com.cloud.consumer.feign.UserFeignClient;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 
 
 @RestController
@@ -16,9 +18,22 @@ public class UserController {
 
     @Autowired
     private UserFeignClient userFeignClient;
+    @Autowired
+    private RestTemplate restTemplate;
 
     @GetMapping("/getUser/{id}")
     public User getUser(@PathVariable Long id){
          return userFeignClient.getUser(id);
+    }
+    
+    @GetMapping("/getName")
+    @HystrixCommand(fallbackMethod = "getNameFallback")
+    public String getName(){
+    	 return restTemplate.getForObject("http://cloud-provider/user/getName",String.class);
+    }
+    
+    public String getNameFallback() {
+//        return restTemplate.getForObject("http://cloud-provider2/user/getName",String.class);
+    	return "王3";
     }
 }
